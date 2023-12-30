38c95c7302a8dec25365618672fbd08807829476
hilomafrancisoliver <hilomafrancisoliver@gmail.com>
2023-12-30 15:44:20 +0800

    Creating Form for Crud

---
+++ application/config/routes.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+
+$route['default_controller'] = 'welcome';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
+
+$route['/'] = 'welcome/index';
